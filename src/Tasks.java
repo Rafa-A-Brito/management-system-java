e0198102672da2ba17554e8516f17121bf2f51e8
Rafa-A-Brito <rafabrito1903@gmail.com>
2025-09-16 17:19:50 -0300

    Atributos e métodos de mudança status / exibir dados

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,3 +1,60 @@
-public class Tasks {
+import java.util.Date;
 
+// Atributtes of class tasks
+
+public class Tasks {
+    String description;
+    Date time;
+    String status;
+    String manager;
+
+
+    // Getters
+     public String getDescription() {
+         return this.description;
+     }
+
+    public Date getDate() {
+        return this.time;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public String getManager() {
+        return this.manager;
+    }
+
+    // Setters
+
+    public void setDescription(String _d){
+         this.description = _d;
+    }
+
+    public void setTime(Date _t){
+        this.time = _t;
+    }
+
+    public void setStatus(String _st){
+        this.status = _st;
+    }
+
+    public void setManager(String _m){
+        this.manager = _m;
+    }
+
+
+
+
+    // MostraDados() method in English
+    public void ShowData(){
+
+    }
+
+    // MudarStatus() method in English
+    public void Change Status(){
+         ""
+    }
 }
+
